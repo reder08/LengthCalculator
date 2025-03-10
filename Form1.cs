b7f2176554329e6ec1294e4840d18f91fd2e37a1
reder08 <jlkimim1008@gmail.com>
2025-03-10 15:13:36 +0800

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LengthCalculator
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtCM_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void btnAllClear_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
