75cd32372e3020fc004c5eac16513a1fdfa8624f
reder08 <jlkimim1008@gmail.com>
2025-03-24 14:46:29 +0800

    修改函式版本

--- Form1.cs
+++ Form1.cs
@@ -20,35 +20,116 @@ namespace LengthCalculator
         // 全域變數
         string strInput; // 字串型態的strInput變數
         double douOutput; // double浮點數型態的douOutput變數
+      
+        string strInput; 
+        double douOutput; 
 
         private void txtCM_KeyUp(object sender, EventArgs e)
         {
-            strInput = txtCM.Text; // 將txtCM文字框的值放入strInput變數
+            strInput = txtCM.Text; 
 
             // 判斷式，如果能夠以double.TryParse成功轉型，那才做數值的計算
+            
             if (double.TryParse(strInput, out douOutput) == true)
             {
-                txtM.Text = string.Format("{0:0.##########}", douOutput / 100);
-                txtKM.Text = string.Format("{0:0.##########}", douOutput / 100000);
-                txtIn.Text = string.Format("{0:0.##########}", douOutput / 2.54);
-                txtFt.Text = string.Format("{0:0.##########}", douOutput / 30.48);
-                txtYard.Text = string.Format("{0:0.##########}", douOutput / 91.44);
+                caculateAnswer(0, douOutput);
             }
             else
             {
+               
                 txtInfo.Text = "請輸入數字";
                 txtCM.Text = "";
+            }
+        }
+
+            // 給予每一個輸入文字框一個「空字串」
+        private void txtM_KeyUp(object sender, EventArgs e)
+        {
+            strInput = txtM.Text; 
+
+           
+            if (double.TryParse(strInput, out douOutput) == true)
+            {
+                caculateAnswer(1, douOutput * 100);
+            }
+            else
+            {
+               
+                txtInfo.Text = "請輸入數字";
                 txtM.Text = "";
+            }
+        }
+
+        private void txtKM_KeyUp(object sender, EventArgs e)
+        {
+            strInput = txtKM.Text; // txtKM
+
+           
+            if (double.TryParse(strInput, out douOutput) == true)
+            {
+                caculateAnswer(2, douOutput * 10000);
+            }
+            else
+            {
+               
+                txtInfo.Text = "請輸入數字";
                 txtKM.Text = "";
+            }
+        }
+
+        private void txtIn_KeyUp(object sender, EventArgs e)
+        {
+            strInput = txtIn.Text; // txtIn
+
+            
+            if (double.TryParse(strInput, out douOutput) == true)
+            {
+                caculateAnswer(3, douOutput * 2.54);
+            }
+            else
+            {
+               
+                txtInfo.Text = "請輸入數字";
                 txtIn.Text = "";
+            }
+        }
+
+        private void txtFt_KeyUp(object sender, EventArgs e)
+        {
+            strInput = txtFt.Text; // txtFt
+
+            
+            if (double.TryParse(strInput, out douOutput) == true)
+            {
+                caculateAnswer(4, douOutput * 30.48);
+            }
+            else
+            {
+               
+                txtInfo.Text = "請輸入數字";
                 txtFt.Text = "";
+            }
+        }
+
+        private void txtYard_KeyUp(object sender, EventArgs e)
+        {
+            strInput = txtYard.Text; // txtFt
+
+            
+            if (double.TryParse(strInput, out douOutput) == true)
+            {
+                caculateAnswer(5, douOutput * 91.44);
+            }
+            else
+            {
+               
+                txtInfo.Text = "請輸入數字";
                 txtYard.Text = "";
             }
         }
 
         private void btnAllClear_Click(object sender, EventArgs e)
         {
-            // 給予每一個輸入文字框一個「空字串」
             txtCM.Text = "";
             txtM.Text = "";
             txtKM.Text = "";
@@ -58,132 +139,21 @@ namespace LengthCalculator
             txtInfo.Text = "";
         }
 
-        private void txtM_KeyUp(object sender, KeyEventArgs e)
+       
+        private void caculateAnswer(int _kind, double _value)
         {
-            double douM;
-
-            douM = Convert.ToDouble(txtM.Text);
-            if (double.TryParse(strInput, out douOutput) == true)
-            {
-                txtCM.Text = string.Format("{0:0.##########}", douM * 100);
-                txtKM.Text = string.Format("{0:0.##########}", douM / 1000);
-                txtIn.Text = string.Format("{0:0.##########}", douM * 100 / 2.54);
-                txtFt.Text = string.Format("{0:0.##########}", douM * 100 / 30.48);
-                txtYard.Text = string.Format("{0:0.##########}", douM * 100 / 91.44);
-            }
-            else
-            {
-                txtInfo.Text = "請輸入數字";
-                txtCM.Text = "";
-                txtM.Text = "";
-                txtKM.Text = "";
-                txtIn.Text = "";
-                txtFt.Text = "";
-                txtYard.Text = "";
-            }
-        }
-
-        private void txtKM_KeyUp(object sender, KeyEventArgs e)
-        {
-            double douKM;
-
-            douKM = Convert.ToDouble(txtKM.Text);
-            if (double.TryParse(strInput, out douOutput) == true)
-            {
-                txtCM.Text = string.Format("{0:0.##########}", douKM * 100000);
-                txtM.Text = string.Format("{0:0.##########}", douKM * 1000);
-                txtIn.Text = string.Format("{0:0.##########}", douKM * 100000 / 2.54);
-                txtFt.Text = string.Format("{0:0.##########}", douKM * 100000 / 30.48);
-                txtYard.Text = string.Format("{0:0.##########}", douKM * 100000 / 91.44);
-            }
-            else
-            {
-                txtInfo.Text = "請輸入數字";
-                txtCM.Text = "";
-                txtM.Text = "";
-                txtKM.Text = "";
-                txtIn.Text = "";
-                txtFt.Text = "";
-                txtYard.Text = "";
-            }
-        }
-
-        private void txtIn_KeyUp(object sender, KeyEventArgs e)
-        {
-            double douIn;
-
-            douIn = Convert.ToDouble(txtIn.Text);
-            if (double.TryParse(strInput, out douOutput) == true)
-            {
-                txtCM.Text = string.Format("{0:0.##########}", douIn * 2.54);
-                txtM.Text = string.Format("{0:0.##########}", douIn * 2.54 / 100);
-                txtKM.Text = string.Format("{0:0.##########}", douIn * 2.54 / 100000);
-                txtFt.Text = string.Format("{0:0.##########}", douIn / 12);
-                txtYard.Text = string.Format("{0:0.##########}", douIn / 36);
-            }
-            else
-            {
-                txtInfo.Text = "請輸入數字";
-                txtCM.Text = "";
-                txtM.Text = "";
-                txtKM.Text = "";
-                txtIn.Text = "";
-                txtFt.Text = "";
-                txtYard.Text = "";
-            }
-        }
-
-        private void txtFt_KeyUp(object sender, KeyEventArgs e)
-        {
-            double douFt;
-
-            douFt = Convert.ToDouble(txtFt.Text);
-            if (double.TryParse(strInput, out douOutput) == true)
-            {
-                txtCM.Text = string.Format("{0:0.##########}", douFt * 30.48);
-                txtM.Text = string.Format("{0:0.##########}", douFt * 30.48 / 100);
-                txtKM.Text = string.Format("{0:0.##########}", douFt * 30.48 / 100000);
-                txtIn.Text = string.Format("{0:0.##########}", douFt * 12);
-                txtYard.Text = string.Format("{0:0.##########}", douFt / 3);
-            }
-            else
-            {
-                txtInfo.Text = "請輸入數字";
-                txtCM.Text = "";
-                txtM.Text = "";
-                txtKM.Text = "";
-                txtIn.Text = "";
-                txtFt.Text = "";
-                txtYard.Text = "";
-            }
-        }
-
-        private void txtYard_KeyUp(object sender, KeyEventArgs e)
-        {
-            double douYard;
-
-            douYard = Convert.ToDouble(txtYard.Text);
-            if (double.TryParse(strInput, out douOutput) == true)
-            {
-                txtCM.Text = string.Format("{0:0.##########}", douYard * 91.44);
-                txtM.Text = string.Format("{0:0.##########}", douYard * 91.44 / 100);
-                txtKM.Text = string.Format("{0:0.##########}", douYard * 91.44 / 100000);
-                txtIn.Text = string.Format("{0:0.##########}", douYard * 36);
-                txtFt.Text = string.Format("{0:0.##########}", douYard * 3);
-            }
-            else
-            {
-                txtInfo.Text = "請輸入數字";
-                txtCM.Text = "";
-                txtM.Text = "";
-                txtKM.Text = "";
-                txtIn.Text = "";
-                txtFt.Text = "";
-                txtYard.Text = "";
-            }
+            if (_kind != 0)
+                txtCM.Text = string.Format("{0:0.##########}", _value);
+            if (_kind != 1)
+                txtM.Text = string.Format("{0:0.##########}", _value / 100);
+            if (_kind != 2)
+                txtKM.Text = string.Format("{0:0.##########}", _value / 100000);
+            if (_kind != 3)
+                txtIn.Text = string.Format("{0:0.##########}", _value / 2.54);
+            if (_kind != 4)
+                txtFt.Text = string.Format("{0:0.##########}", _value / 30.48);
+            if (_kind != 5)
+                txtYard.Text = string.Format("{0:0.##########}", _value / 91.44);
         }
     }
 }
-         
-    
-
